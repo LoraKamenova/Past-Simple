acd6bdaefcf0d96f3aef053240fda3ed6ecc0a45
Lora <cruzeiro@abv.bg>
2021-07-08 21:00:43 +0300

    added links to home page cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { NavigationComponent } from './components/navigation/navigation/navigation.component';
 import { FooterComponent } from './components/footer/footer/footer.component';
-import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    FooterComponent,
-    IndexComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
