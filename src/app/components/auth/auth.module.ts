33ce1362e05a65dc459ed2c076ab6d5775374c4b
Lora <cruzeiro@abv.bg>
2021-07-09 22:42:22 +0300

    login page css fix

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
     RegisterComponent,
     LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class AuthModule { }
