737b97956f5bd6287bb38d068507093d7b7c4b1d
Lora <cruzeiro@abv.bg>
2021-07-10 19:50:29 +0300

    slogans page fix

--- src/app/components/entertainment/entertainment.module.ts
+++ src/app/components/entertainment/entertainment.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EntertainmentCategoriesComponent } from './entertainment-categories/entertainment-categories.component';
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
   declarations: [EntertainmentCategoriesComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        RouterModule
+    ]
 })
 export class EntertainmentModule { }
--- src/app/components/venue/venue.module.ts
+++ src/app/components/venue/venue.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VenueCategoriesComponent } from './venue-categories/venue-categories.component';
+import {AppRoutingModule} from "../../app-routing.module";
 
 
 
 @NgModule({
   declarations: [VenueCategoriesComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        AppRoutingModule
+    ]
 })
 export class VenueModule { }
