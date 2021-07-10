33e24717b3cefdb34231ca2180c2c62a50842892
Lora <cruzeiro@abv.bg>
2021-07-10 12:43:37 +0300

    modules structure fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/navigation/navigation/navigation.component';
-import { FooterComponent } from './components/footer/footer/footer.component';
-import { AuthModule } from "./components/auth/auth.module";
-import { HomeComponent } from "./components/home/home.component";
-import { SloganModule } from "./components/slogan/slogan.module";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './components/navigation/navigation/navigation.component';
+import {FooterComponent} from './components/footer/footer/footer.component';
+import {AuthModule} from "./components/auth/auth.module";
+import {HomeComponent} from "./components/home/home.component";
+import {SloganModule} from "./components/slogan/slogan.module";
 import {BlogModule} from "./components/blog/blog.module";
 import {MemoryModule} from "./components/memory/memory.module";
 import {ProfileModule} from "./components/profile/profile.module";
 import {QuizModule} from "./components/quiz/quiz.module";
+import {AdminPanelModule} from "./components/admin-panel/admin-panel.module";
+import {EntertainmentModule} from "./components/entertainment/entertainment.module";
+import {TravelModule} from "./components/travel/travel.module";
+import {VenueModule} from "./components/venue/venue.module";
+import {PlacesComponent} from "./components/places/places.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    PlacesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AdminPanelModule,
     AuthModule,
-    SloganModule,
     BlogModule,
+    EntertainmentModule,
     MemoryModule,
     ProfileModule,
-    QuizModule
+    QuizModule,
+    SloganModule,
+    TravelModule,
+    VenueModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/travel/travel.module.ts
+++ src/app/components/travel/travel.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TravelCategoriesComponent } from './travel-categories/travel-categories.component';
-import { SeaCategotiesComponent } from './sea-categoties/sea-categoties.component';
-import { SeaCategoriesComponent } from './sea-categories/sea-categories.component';
-
 
 
 @NgModule({
-  declarations: [TravelCategoriesComponent, SeaCategotiesComponent, SeaCategoriesComponent],
+  declarations: [TravelCategoriesComponent],
   imports: [
     CommonModule
   ]
