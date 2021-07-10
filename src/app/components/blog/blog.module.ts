ef706a4e0b58446c86406b5ee91e39b4b465aa63
Lora <cruzeiro@abv.bg>
2021-07-10 09:38:20 +0300

    added admin module

--- src/app/components/blog/blog.module.ts
+++ src/app/components/blog/blog.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogPostsComponent } from './blog-posts/blog-posts.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { DeletePostComponent } from './delete-post/delete-post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { PostComponent } from './post/post.component';
 
 
 
 @NgModule({
-  declarations: [BlogPostsComponent],
+  declarations: [BlogPostsComponent, AddPostComponent, DeletePostComponent, EditPostComponent, PostComponent],
   imports: [
     CommonModule
   ]
--- src/app/components/slogan/slogan.module.ts
+++ src/app/components/slogan/slogan.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllSlogansComponent } from './all-slogans/all-slogans.component';
+import { AddSloganComponent } from './add-slogan/add-slogan.component';
 
 
 
 @NgModule({
-  declarations: [AllSlogansComponent],
+  declarations: [AllSlogansComponent, AddSloganComponent],
   imports: [
     CommonModule
   ]
