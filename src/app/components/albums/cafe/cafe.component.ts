dd229fc38413914f93b5e0f13b681352e8b08234
Lora <cruzeiro@abv.bg>
2021-08-07 22:30:46 +0300

    gallery page css fix

--- src/app/components/albums/cafe/cafe.component.ts
+++ src/app/components/albums/cafe/cafe.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+
+export const fade = trigger('fade', [
+  state('inactive', style({ opacity: 0 })),
+  state('active', style({ opacity: 1 })),
+  transition('* <=> *', [
+    animate(1500)
+  ])
+]);
 
 @Component({
   selector: 'app-cafe',
   templateUrl: './cafe.component.html',
-  styleUrls: ['./cafe.component.css']
+  styleUrls: ['./cafe.component.css'],
+  animations: [fade]
 })
+
+
 export class CafeComponent implements OnInit {
+  times = 15;
+  counter = 0;
+  state;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDone($event) {
+    // call this function at the end of the previous animation.
+    // run it as many time as defined
+    if (this.counter < this.times) {
+      this.state = this.state === 'active' ? 'inactive' : 'active';
+      this.counter++;
+    }
   }
 
 }
