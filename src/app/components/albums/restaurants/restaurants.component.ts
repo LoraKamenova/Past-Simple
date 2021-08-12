01fe2441b1368cf87c9acf594a617bc8d5c118a7
Lora <cruzeiro@abv.bg>
2021-08-12 23:26:00 +0300

    gallery pages fix

--- src/app/components/albums/restaurants/restaurants.component.ts
+++ src/app/components/albums/restaurants/restaurants.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import {Observable} from "rxjs";
+import {Picture} from "../../../models/picture";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PictureService} from "../../../services/picture.service";
+
+export const fade = trigger('fade', [
+  state('inactive', style({ opacity: 0 })),
+  state('active', style({ opacity: 1 })),
+  transition('* <=> *', [
+    animate(2500)
+  ])
+]);
 
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  styleUrls: ['./restaurants.component.css'],
+  animations: [fade]
 })
 export class RestaurantsComponent implements OnInit {
+  picture$: Observable<Array<Picture>>;
 
-  constructor() { }
+  times = 999;
+  counter = 0;
+  state;
+
+  constructor(private route: ActivatedRoute, private pictureService: PictureService, private router: Router) { }
 
   ngOnInit(): void {
+    this.picture$ = this.pictureService.getRestaurantsAlbum();
+    console.log(this.picture$)
+  }
+
+  onDone($event) {
+    if (this.counter < this.times) {
+      this.state = this.state === 'active' ? 'inactive' : 'active';
+      this.counter++;
+    }
   }
 
 }
