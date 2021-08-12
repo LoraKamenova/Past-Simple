73ebe14ee311133dc6afb4409319f417e482d329
Lora <cruzeiro@abv.bg>
2021-08-12 21:53:22 +0300

    gallery pages fix

--- src/app/components/albums/cinema/cinema.component.ts
+++ src/app/components/albums/cinema/cinema.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import {Observable} from "rxjs";
+import {Picture} from "../../../models/picture";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PictureService} from "../../../services/picture.service";
+
+export const fade = trigger('fade', [
+  state('inactive', style({ opacity: 0 })),
+  state('active', style({ opacity: 1 })),
+  transition('* <=> *', [
+    animate(2500)
+  ])
+]);
 
 @Component({
   selector: 'app-cinema',
   templateUrl: './cinema.component.html',
-  styleUrls: ['./cinema.component.css']
+  styleUrls: ['./cinema.component.css'],
+  animations: [fade]
 })
 export class CinemaComponent implements OnInit {
+  picture$: Observable<Array<Picture>>;
 
-  constructor() { }
+  times = 999;
+  counter = 0;
+  state;
+
+  constructor(private route: ActivatedRoute, private pictureService: PictureService, private router: Router) { }
 
   ngOnInit(): void {
+    this.picture$ = this.pictureService.getCinemaAlbum();
+    console.log(this.picture$)
+  }
+
+  onDone($event) {
+    if (this.counter < this.times) {
+      this.state = this.state === 'active' ? 'inactive' : 'active';
+      this.counter++;
+    }
   }
 
 }
--- src/app/components/albums/hotels/hotels.component.ts
+++ src/app/components/albums/hotels/hotels.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import {Observable} from "rxjs";
+import {Picture} from "../../../models/picture";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PictureService} from "../../../services/picture.service";
+
+export const fade = trigger('fade', [
+  state('inactive', style({ opacity: 0 })),
+  state('active', style({ opacity: 1 })),
+  transition('* <=> *', [
+    animate(2500)
+  ])
+]);
+
 
 @Component({
   selector: 'app-hotels',
   templateUrl: './hotels.component.html',
-  styleUrls: ['./hotels.component.css']
+  styleUrls: ['./hotels.component.css'],
+  animations: [fade]
 })
 export class HotelsComponent implements OnInit {
+  picture$: Observable<Array<Picture>>;
 
-  constructor() { }
+  times = 999;
+  counter = 0;
+  state;
+
+  constructor(private route: ActivatedRoute, private pictureService: PictureService, private router: Router) { }
 
   ngOnInit(): void {
+    this.picture$ = this.pictureService.getHotelsAlbum();
+    console.log(this.picture$)
+  }
+
+  onDone($event) {
+    if (this.counter < this.times) {
+      this.state = this.state === 'active' ? 'inactive' : 'active';
+      this.counter++;
+    }
   }
 
 }
