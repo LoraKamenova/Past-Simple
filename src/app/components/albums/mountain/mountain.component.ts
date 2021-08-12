415eb0d096257582b087859d9fc3997fb6efcee4
Lora <cruzeiro@abv.bg>
2021-08-12 22:33:44 +0300

    gallery pages fix

--- src/app/components/albums/mountain/mountain.component.ts
+++ src/app/components/albums/mountain/mountain.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+import {Observable} from "rxjs";
+import {Picture} from "../../../models/picture";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PictureService} from "../../../services/picture.service";
+
+export const fade = trigger('fade', [
+  state('inactive', style({ opacity: 0 })),
+  state('active', style({ opacity: 1 })),
+  transition('* <=> *', [
+    animate(2500)
+  ])
+]);
 
 @Component({
   selector: 'app-mountain',
   templateUrl: './mountain.component.html',
-  styleUrls: ['./mountain.component.css']
+  styleUrls: ['./mountain.component.css'],
+  animations: [fade]
 })
 export class MountainComponent implements OnInit {
+  picture$: Observable<Array<Picture>>;
 
-  constructor() { }
+  times = 999;
+  counter = 0;
+  state;
+
+  constructor(private route: ActivatedRoute, private pictureService: PictureService, private router: Router) { }
 
   ngOnInit(): void {
+    this.picture$ = this.pictureService.getMountainAlbum();
+    console.log(this.picture$)
   }
 
+  onDone($event) {
+    if (this.counter < this.times) {
+      this.state = this.state === 'active' ? 'inactive' : 'active';
+      this.counter++;
+    }
+  }
 }
